b10a844f87c61681ad1b395c6ceee504fa85387d
Ash T <ashtan19@gmail.com>
2020-05-05 15:49:21 -0700

    LC 136

---
+++ LEETCODE/136.py
@@ -0,0 +1,22 @@
+# Leetcode: 136 Single Number in Array
+
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+# Solving process:
+# Problems Encountered: 
+
+# Other Solutions: Use Hash Table -> but extra mem , use Math or Bit Wise 
+
+class Solution:
+    def singleNumber(self, nums):
+        nums.sort()
+        i = 0 
+        while i < len(nums)-1:
+            if nums[i] != nums[i+1]: return nums[i]
+            i += 2
+        return nums.pop()
+
+
+testSol = Solution()
+testArray = [4,1,2,1,2]
+print (testSol.singleNumber(testArray))
