029c6cb873f1e7d33b97a17b16aab227c11b5136
Ash T <ashtan19@gmail.com>
2020-05-31 15:19:37 -0700

    LC BTree Max Depth Attempt 2

--- LEETCODE/Easy/104-BtreeMaxDepth.py
+++ LEETCODE/Easy/104-BtreeMaxDepth.py
@@ -1,9 +1,11 @@
 # Leetcode: 104 Max Depth of BTree
 
+# Attempts: 2
+
 # Time Complexity: O(n)
-# Space Complexity: 
+# Space Complexity: O(n)
 # Solving process:
-# Problems Encountered: 
+# Problems Encountered:
 
 # Other Solutions:
 
@@ -13,10 +15,27 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+
 class Solution:
     def maxDepth(self, root: TreeNode) -> int:
-        if root == None: return 0
-        
+        if root == None:
+            return 0
+
         leftMax = self.maxDepth(root.left)
         rightMax = self.maxDepth(root.right)
-        return max(leftMax, rightMax) +1
+        return max(leftMax, rightMax) + 1
+
+# Attempt 2
+
+
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if root == None:
+            return 0
+
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
