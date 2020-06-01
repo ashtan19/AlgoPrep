5a8bb01afae5f6c478e7bedbbbd5a41242a5c40f
Ash T <ashtan19@gmail.com>
2020-06-01 13:39:38 -0700

    LC 448 Find Missing Nums Attempt 2

--- LEETCODE/Easy/448-FindMissingNums.py
+++ LEETCODE/Easy/448-FindMissingNums.py
@@ -1,13 +1,16 @@
 # Leetcode: 448 Find Dissapeared Numbers
 
+# Attempts: 2
+
 # Time Complexity: O(n)
-# Space Complexity: O(n)
+# Space Complexity: O(n) O(1)
 # Solving process:
-# Problems Encountered: 
+# Problems Encountered:
 
 # Other Solutions: Mark the numbers with -1, then run through array again to find indexes that are not negated
 
 import collections
+
 
 class Solution:
     def findDisappearedNumbers(self, nums):
@@ -21,7 +24,7 @@ class Solution:
 
 
 testSolution = Solution()
-print(testSolution.findDisappearedNumbers([4,3,2,7,8,2,3,1]))
+print(testSolution.findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1]))
 
 
 # Second Solution
@@ -32,10 +35,29 @@ class Solution:
             temp = abs(nums[i]) - 1
             if nums[temp] > 0:
                 nums[temp] *= -1
-        
+
         res = []
-        for i,n in enumerate(nums):
+        for i, n in enumerate(nums):
             if n > 0:
                 res.append(i+1)
-        
+
         return res
+
+# Attempt 2
+
+
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            pointer = abs(nums[i])-1
+            if nums[pointer] > 0:
+                nums[pointer] *= -1
+        result = []
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                result.append(i+1)
+        return result
