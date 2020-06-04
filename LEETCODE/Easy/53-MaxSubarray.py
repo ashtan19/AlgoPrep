89c612060e75ec824057b2ef629352fb7a504804
Ash T <ashtan19@gmail.com>
2020-06-04 13:31:41 -0700

    LC 53 Max Sub array Attempt 2

--- LEETCODE/Easy/53-MaxSubarray.py
+++ LEETCODE/Easy/53-MaxSubarray.py
@@ -1,11 +1,36 @@
 # Leetcode: 53 Largest SubArray (Did not finish - must try again)
 
+# Attempts: 2
+# Completed: Y
+# Ideal: Y
+
 # Time Complexity: O(n)
 # Space Complexity: O(1)
 # Solving process: Sum it up iteratively starting from element 1. If the cur element is bigger than current sum then ditch the current sum
 # Problems Encountered: Thought Process was difficult
 
 # Other Solutions: Divide and conquer
+
+# Attempt 2. Passed
+
+
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if nums == None or nums == []:
+            return None
+        maxsum, currsum = nums[0], nums[0]
+
+        for i in range(1, len(nums)):
+            currsum += nums[i]
+            if currsum < nums[i]:
+                currsum = nums[i]
+            if currsum > maxsum:
+                maxsum = currsum
+        return maxsum
 
 
 class Solution(object):
