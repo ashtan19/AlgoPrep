5bf0b65603ed21b685e86002c55114927913ddca
Ash T <ashtan19@gmail.com>
2020-06-04 14:52:32 -0700

    LC 155  Min stack

---
+++ LEETCODE/Easy/155-MinStack.py
@@ -0,0 +1,52 @@
+# Leetcode:155. Min Stack
+
+# Attempts: 1
+# Completed: N
+# Acheived Ideal:
+
+# Time Complexity: O(1)
+# Space Complexity: O(n)
+
+# Solving process: Store current min with new value
+# Problems Encountered:
+
+# Other Solutions:
+
+
+class MinStack:
+
+
+def __init__(self):
+    self.q = []
+
+# @param x, an integer
+# @return an integer
+
+
+def push(self, x):
+    curMin = self.getMin()
+    if curMin == None or x < curMin:
+        curMin = x
+    self.q.append((x, curMin))
+
+# @return nothing
+
+
+def pop(self):
+    self.q.pop()
+
+
+# @return an integer
+def top(self):
+    if len(self.q) == 0:
+        return None
+    else:
+        return self.q[len(self.q) - 1][0]
+
+
+# @return an integer
+def getMin(self):
+    if len(self.q) == 0:
+        return None
+    else:
+        return self.q[len(self.q) - 1][1]
