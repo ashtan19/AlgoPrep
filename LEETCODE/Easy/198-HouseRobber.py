a40453ee921deb45454dd59b4ba94d3fd6ef9449
Ash T <ashtan19@gmail.com>
2020-06-08 13:40:59 -0700

    LC 198 House Robber

--- LEETCODE/Easy/198-HouseRobber.py
+++ LEETCODE/Easy/198-HouseRobber.py
@@ -1,5 +1,7 @@
 # Leetcode: 198 House Robber - Try Again
 
+# Attempts: 2
+
 # Time Complexity: O(2^n) , O(n) , O(n)
 # Space Complexity: O(n), O(n), O(1)
 # Solving process:
@@ -23,7 +25,7 @@ class Solution(object):
             return 0
         return max(self.robHelper(nums, i-2)+nums[i], self.robHelper(nums, i-1))
 
-# Memoization
+# DP
 
 
 class Solution(object):
@@ -32,35 +34,36 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        self.memo = [-1]*len(nums)
-        return self.robHelper(nums, len(nums)-1)
-
-    def robHelper(self, nums, i):
-        if (i < 0):
+        if nums == None or nums == []:
             return 0
-        if self.memo[i] >= 0:
-            return self.memo[i]
-        result = max(self.robHelper(nums, i-2) +
-                     nums[i], self.robHelper(nums, i-1))
-        self.memo[i] = result
-        return result
-
-#Iterative + memo
-
-
-class Solution(object):
-    def rob(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        if len(nums) == 0:
-            return 0
-        memo = [-1]*(len(nums)+1)
+        if len(nums) == 1:
+            return nums[0]
+        if len(nums) == 2:
+            return max(nums)
+        memo = [None] * (len(nums)+1)
         memo[0] = 0
         memo[1] = nums[0]
-
-        for i in range(1, len(nums)):
-            val = nums[i]
-            memo[i+1] = max(memo[i], memo[i-1]+val)
+        i = 1
+        while i < len(nums):
+            memo[i+1] = max(memo[i-1]+nums[i], memo[i])
+            i += 1
         return memo[len(nums)]
+
+# Static DP
+
+
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if nums == None or nums == []:
+            return 0
+        prev1 = 0
+        prev2 = 0
+        for num in nums:
+            tmp = prev1
+            prev1 = max(prev1, prev2+num)
+            prev2 = tmp
+        return prev1
