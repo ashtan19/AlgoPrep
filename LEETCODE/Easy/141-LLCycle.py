c8998347e9c70afdcf22fbbb0d5cb32da59695e6
Ash T <ashtan19@gmail.com>
2020-06-07 21:28:15 -0700

    LC 141 LL Cycle Attempt 2

--- LEETCODE/Easy/141-LLCycle.py
+++ LEETCODE/Easy/141-LLCycle.py
@@ -1,5 +1,7 @@
 # Leetcode: 141 Linked List Cycle
 
+# Attempts : 2
+
 # Time Complexity: O(n)
 # Space Complexity: O(n)
 # Solving process:
@@ -23,5 +25,26 @@ class Solution(object):
             else:
                 nodesSeen[head] = None
             head = head.next
+
+        return False
+
+# Attempt 2
+
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+
+        fast = head
+        slow = head
+
+        while fast and fast.next and fast.next.next and slow and slow.next:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                return True
 
         return False
