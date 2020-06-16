b2c3f20f9f3964c31913b652dd01755906edcdbf
Ash T <ashtan19@gmail.com>
2020-06-16 13:51:32 -0700

    EDU Max Subarray of Size k

---
+++ EDUCATIVE/CODINGPATTERNS/SLIDINGWINDOW/MaxSubarrayOfSizeK.py
@@ -0,0 +1,12 @@
+def max_sub_array_of_size_k(k, arr):
+    # TODO: Write your code here
+    maxSum = -float('inf')
+    windowSum = 0
+    windowStart = 0
+    for windowEnd in range(len(arr)):
+        windowSum += arr[windowEnd]
+        if windowEnd >= k-1:
+            maxSum = max(windowSum, maxSum)
+            windowSum -= arr[windowStart]
+            windowStart += 1
+    return maxSum
