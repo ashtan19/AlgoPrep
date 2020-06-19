cf86f0334191449d5c17ce0f4df792a1dd0fc775
Ash T <ashtan19@gmail.com>
2020-06-19 15:19:10 -0700

    EDU Merge Intervals

---
+++ EDUCATIVE/CODINGPATTERNS/MERGEINTERVALS/MergeIntervals.py
@@ -0,0 +1,44 @@
+'''
+Question: 
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+Pattern: Merge Intervals 
+Technique: Sort Intervals then merge
+
+
+'''
+
+
+class Interval:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+    def print_interval(self):
+        print("[" + str(self.start) + ", " + str(self.end) + "]", end='')
+
+
+def merge(intervals):
+    if len(intervals) < 2:
+        return intervals
+
+    # sort the intervals on the start time
+    intervals.sort(key=lambda x: x.start)
+
+    mergedIntervals = []
+    start = intervals[0].start
+    end = intervals[0].end
+    for i in range(1, len(intervals)):
+        interval = intervals[i]
+        if interval.start <= end:  # overlapping intervals, adjust the 'end'
+            end = max(interval.end, end)
+        else:  # non-overlapping interval, add the previous internval and reset
+            mergedIntervals.append(Interval(start, end))
+            start = interval.start
+            end = interval.end
+
+    # add the last interval
+    mergedIntervals.append(Interval(start, end))
+    return mergedIntervals
