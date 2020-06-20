7c191fb3ff6a2b87e8632695c455986948256b23
Ash T <ashtan19@gmail.com>
2020-06-20 15:03:48 -0700

    EDU Insert Interval Better Solution

--- EDUCATIVE/CODINGPATTERNS/MERGEINTERVALS/InsertInterval.py
+++ EDUCATIVE/CODINGPATTERNS/MERGEINTERVALS/InsertInterval.py
@@ -5,10 +5,12 @@ Time Complexity: O(n)
 Space Complexity: O(n)
 
 Pattern: Merge Intervals
-Technique: 
+Technique: If Intervals are already sorted, just figure out when to consider the new interval
 
 
 '''
+
+# O(nlogn) b/c of the sort
 
 
 def insert(intervals, new_interval):
@@ -26,4 +28,31 @@ def insert(intervals, new_interval):
             if end > merged_end:
                 merged[-1][1] = end
 
+    return merged
+
+# O(n)
+
+
+def insert(intervals, new_interval):
+    merged = []
+    i, start, end = 0, 0, 1
+
+    # skip (and add to output) all intervals that come before the 'new_interval'
+    while i < len(intervals) and intervals[i][end] < new_interval[start]:
+        merged.append(intervals[i])
+        i += 1
+
+    # merge all intervals that overlap with 'new_interval'
+    while i < len(intervals) and intervals[i][start] <= new_interval[end]:
+        new_interval[start] = min(intervals[i][start], new_interval[start])
+        new_interval[end] = max(intervals[i][end], new_interval[end])
+        i += 1
+
+    # insert the new_interval
+    merged.append(new_interval)
+
+    # add all the remaining intervals to the output
+    while i < len(intervals):
+        merged.append(intervals[i])
+        i += 1
     return merged
