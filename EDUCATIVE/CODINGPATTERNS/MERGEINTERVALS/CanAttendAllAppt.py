290d663ca0f0bbdab03a1e1316c4611dced29f7a
Ash T <ashtan19@gmail.com>
2020-06-21 14:01:29 -0700

    EDU Conflict of Appts

---
+++ EDUCATIVE/CODINGPATTERNS/MERGEINTERVALS/CanAttendAllAppt.py
@@ -0,0 +1,23 @@
+'''
+Question: Conflicting Appointments (medium)
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+Pattern: Merge Intervals 
+Technique: Sort Intervals by start, Check if start is before previous end
+
+
+'''
+
+
+def can_attend_all_appointments(intervals):
+    # TODO: Write your code here
+    intervals.sort(key=lambda x: x[0])
+
+    for i in range(1, len(intervals)):
+        prev_start, prev_end = intervals[i-1]
+        cur_start, cur_end = intervals[i]
+        if cur_start < prev_end:
+            return False
+    return True
