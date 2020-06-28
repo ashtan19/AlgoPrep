23e0ae93b01fb4aafad5b7034b705571cf246317
Ash Tan <ashtan@Ashs-MacBook-Pro.local>
2020-06-27 20:05:32 -0700

    EDU BSearch

---
+++ EDUCATIVE/CODINGPATTERNS/BINARYSEARCH/OrderAgnosticBSearch.py
@@ -0,0 +1,35 @@
+"""
+Question: Order-agnostic Binary Search (easy)
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+Pattern: Binary Search
+Technique: 
+
+"""
+
+
+def binary_search(arr, key):
+    start, end = 0, len(arr) - 1
+    isAscending = arr[start] < arr[end]
+    while start <= end:
+        # calculate the middle of the current range
+        mid = start + (end - start) // 2
+
+        if key == arr[mid]:
+            return mid
+
+        if isAscending:  # ascending order
+            if key < arr[mid]:
+                end = mid - 1  # the 'key' can be in the first half
+            else:  # key > arr[mid]
+                start = mid + 1  # the 'key' can be in the second half
+        else:  # descending order
+            if key > arr[mid]:
+                end = mid - 1  # the 'key' can be in the first half
+            else:  # key < arr[mid]
+                start = mid + 1  # the 'key' can be in the second half
+
+    return -1  # element not found
+
