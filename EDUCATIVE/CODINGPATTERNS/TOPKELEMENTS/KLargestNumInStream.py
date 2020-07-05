e84f0e686149a4f3991617ec03a7b79b1a3dfb16
ashtan19 <ashtan19@gmail.com>
2020-07-05 14:20:02 -0700

    EDU K Largest Elements in a Stream

---
+++ EDUCATIVE/CODINGPATTERNS/TOPKELEMENTS/KLargestNumInStream.py
@@ -0,0 +1,35 @@
+"""
+Question: Kth Largest Number in a Stream (medium)
+
+Time Complexity: O(nlogk)
+Space Complexity: O(n)
+
+Pattern: Top K Elements 
+Technique: Same 
+
+"""
+
+
+from heapq import heappush, heappop
+
+
+class KthLargestNumberInStream:
+    def __init__(self, nums, k):
+        # TODO: Write your code here
+        self.min_heap = []
+        for i in range(k):
+            heappush(self.min_heap, nums[i])
+
+        for i in range(k, len(nums)):
+            if nums[i] > self.min_heap[0]:
+                heappop(self.min_heap)
+                heappush(self.min_heap, nums[i])
+        self.k = k
+
+    def add(self, num):
+        # TODO: Write your code here
+        if num > self.min_heap[0]:
+            heappop(self.min_heap)
+            heappush(self.min_heap, num)
+        return self.min_heap[0]
+
