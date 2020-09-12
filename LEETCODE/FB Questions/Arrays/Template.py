581133fb0add4a3f1b6bf6cce6c7b688518a809a
ashtan19 <ashtan19@gmail.com>
2020-09-11 17:17:02 -0700

    FB Q Template

---
+++ LEETCODE/FB Questions/Arrays/Template.py
@@ -0,0 +1,18 @@
+"""
+Leetcode:
+
+Attempts:
+Completed:
+Acheived Ideal:
+Under 30 Mins: 
+
+Time Complexity:
+Space Complexity:
+
+Pattern: 
+Technique: 
+
+Problems Encountered:
+Other Solutions:
+
+"""
