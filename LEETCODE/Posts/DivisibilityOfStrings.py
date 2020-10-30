460b7bf28a37ddd09d3ef588412730dd736892a3
ashtan19 <ashtan19@gmail.com>
2020-10-29 22:44:27 -0700

    LC Divisibility of Strings

---
+++ LEETCODE/Posts/DivisibilityOfStrings.py
@@ -0,0 +1,19 @@
+"""
+Leetcode: Amazon OA Divisibility of Strings
+Link: https://leetcode.com/discuss/general-discussion/680341/divisibility-of-strings
+
+Attempts: 1
+Completed: N, did not get most optimal solution
+Acheived Ideal:
+Under 30 Mins: 
+
+Time Complexity: O(s) and then O(t^2)
+Space Complexity: O(1)
+
+Pattern: Checking for factors and patterns
+Technique: Check if t can mod into s and then check iteratively check which size mod is smallest factor of t
+
+Problems Encountered:
+Other Solutions:
+
+"""
