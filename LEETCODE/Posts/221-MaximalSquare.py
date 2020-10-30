008d8019d5aa1fa41e18ea83d899b00d4f8713ff
ashtan19 <ashtan19@gmail.com>
2020-10-30 16:01:22 -0700

    LC Unique Device Names

--- LEETCODE/Posts/221-MaximalSquare.py
+++ LEETCODE/Posts/221-MaximalSquare.py
@@ -9,7 +9,7 @@ Under 30 Mins:
 Time Complexity: O(n*m)
 Space Complexity: O(n*m) but can be O(n) if you use 1 dp array
 
-Pattern: 
+Pattern: Create a dp array and take the min of the element's left , top and top left diagonal and plus 1
 Technique: 
 
 Problems Encountered:
---
+++ LEETCODE/Posts/UniqueDeviceNames.py
@@ -0,0 +1,19 @@
+"""
+Leetcode: Amazon OA Unique Device Names
+Link: https://leetcode.com/discuss/interview-question/896563/
+
+Attempts: 1
+Completed: N but got concept
+Acheived Ideal:
+Under 30 Mins: 
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+
+Pattern: 
+Technique: Use hashtable
+
+Problems Encountered:
+Other Solutions:
+
+"""
