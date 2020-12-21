6c2f18b8074037bdee494dfa40ddc65b5819c8fe
ashtan19 <ashtan19@gmail.com>
2020-12-20 16:27:09 -0800

    LC 242 Valid Anagram

---
+++ LEETCODE/Google Questions/242-ValidAnagram.py
@@ -0,0 +1,29 @@
+"""
+Leetcode: 242. Valid Anagram
+
+Attempts: 1
+Completed: Y
+Acheived Ideal: Y use hashtable for O(n) but extra space
+Under 30 Mins: Y
+
+Time Complexity: O(nlogn)
+Space Complexity:
+
+Pattern: 
+Technique: 
+
+Problems Encountered:
+Other Solutions:
+
+"""
+
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+
+        return sorted(s) == sorted(t)
