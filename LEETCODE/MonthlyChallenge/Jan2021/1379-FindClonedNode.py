60a1d2169d249436f6075a7ffce45dc909beec60
ashtan19 <ashtan19@gmail.com>
2021-01-25 19:43:04 -0800

    LC 1379 Jan Challenge DFS

--- LEETCODE/MonthlyChallenge/Jan2021/1379-FindClonedNode.py
+++ LEETCODE/MonthlyChallenge/Jan2021/1379-FindClonedNode.py
@@ -58,22 +58,22 @@ class Solution(object):
 
 # DFS solution
 class Solution:
-    def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
+    def getTargetCopy(self, original, cloned, target):
         stack_o, stack_c = [], []
+
         node_o, node_c = original, cloned
 
-        while stack_o or node_c:
+        while stack_o or node_o:
             while node_o:
                 stack_o.append(node_o)
                 stack_c.append(node_c)
-
                 node_o = node_o.left
                 node_c = node_c.left
 
             node_o = stack_o.pop()
             node_c = stack_c.pop()
 
-            if node_o is target:
+            if node_o == target:
                 return node_c
 
             node_o = node_o.right
